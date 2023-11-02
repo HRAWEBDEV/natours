12281d44f7a08a651ddc79cf90ea715d39e623f0
hamidwise1 <hrawebdev@gmail.com>
2023-11-02 19:03:58 +0330

    add typescript config and concurrently package

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const serverPort = 3000;
+
+app.listen(serverPort, () => {
+  console.log(`server is listening to port :${serverPort}`);
+});
