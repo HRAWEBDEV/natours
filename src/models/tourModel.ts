e420b1b50697cde0a328791c85dd43a32ebbda2b
hamidwise1 <hrawebdev@gmail.com>
2023-11-14 18:05:12 +0330

    add tour schema properties

--- src/models/tourModel.ts
+++ src/models/tourModel.ts
@@ -1,6 +1,14 @@
 import { Schema, model } from 'mongoose';
 
-const tourSchema = new Schema({});
+const tourSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'a tour name must be provided'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.6 },
+  price: { type: Number, required: [true, 'a tour must have price'] },
+});
 
 const Tour = model('Tour', tourSchema);
 export { Tour };
